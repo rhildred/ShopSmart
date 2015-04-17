0687ed2c10f3eac41749055735c67832e0a19673
rhildred <rhildred@gmail.com>
2015-04-17 09:29:26 -0400

    started to add the model

---
+++ ShopSmart/Models/Product.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ShopSmart.Models
+{
+    /// <summary>
+    /// follows the semantics of a product https://developers.google.com/structured-data/rich-snippets/products
+    /// </summary>
+    public class Product
+    {
+        public string gtin13 { get; set; }
+        public string sku { get; set; }
+        public string name { get; set; }
+        /// <summary>
+        /// the image is actually a url
+        /// </summary>
+        public string image { get; set; }
+        public string description { get; set; }
+        public string brand { get; set; }
+    }
+}
