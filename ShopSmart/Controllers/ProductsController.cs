9245dd582ad034c850f5ed18c1712b941c225633
rhildred <rhildred@gmail.com>
2015-04-17 11:21:38 -0400

    started on the entity framework part

---
+++ ShopSmart/Controllers/ProductsController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.Entity;
+using ShopSmart.Models;
+
+
+namespace ShopSmart.Controllers
+{
+    /// <summary>
+    /// we used the example https://msdn.microsoft.com/en-ca/data/jj193542.aspx to do this project
+    /// </summary>
+    /// 
+
+    public class ProductsContext : DbContext
+    {
+        public ProductsContext() : base("Products") { }
+        public DbSet<Product> Products { get; set; }
+    }
+
+    public class ProductsController : Controller
+    {
+        //
+        // GET: /Products/
+
+        public ActionResult Show()
+        {
+            var db = new ProductsContext();
+            return View(db.Products.ToList());
+        }
+
+    }
+}
