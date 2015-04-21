0941de2142ee54c04c6292b55d97eaebc7674530
rhildred <rhildred@gmail.com>
2015-04-21 10:18:52 -0400

    removed unwanted files

--- ShopSmart/App_Start/WebApiConfig.cs
+++ ShopSmart/App_Start/WebApiConfig.cs
@@ -14,6 +14,7 @@ namespace ShopSmart
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+            config.Formatters.Remove(config.Formatters.XmlFormatter);
         }
     }
 }
---
+++ ShopSmart/Controllers/CatalogController.cs
@@ -0,0 +1,21 @@
+﻿using ShopSmart.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ShopSmart.Controllers
+{
+    public class CatalogController : ApiController
+    {
+        private ProductsContext db = new ProductsContext();
+        // GET api/Products
+        public IEnumerable<Product> GetProducts()
+        {
+            return db.Products;
+        }
+ 
+    }
+}
