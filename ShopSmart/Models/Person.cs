04ef4963f47163a7f27d3b276bb24c85ea4ec69f
rhildred <rhildred@gmail.com>
2015-04-27 12:56:33 -0400

    added new code to validate mail code before adding a user

--- ShopSmart/Models/Person.cs
+++ ShopSmart/Models/Person.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
@@ -14,6 +15,8 @@ namespace ShopSmart.Models
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Email { get; set; }
+        [NotMapped]
+        public string EmailCode { get; set; }
         public Place Place { get; set; }
 
     }
